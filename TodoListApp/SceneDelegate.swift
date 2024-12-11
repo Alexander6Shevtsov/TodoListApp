1e8c47d593591e9271e1dbde1315b7acbd751cbc
Alexander6Shevtsov <axrsvr@gmail.com>
2024-12-11 12:41:16 +0400

    Initial Commit

---
+++ TodoListApp/SceneDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  SceneDelegate.swift
+//  TodoListApp
+//
+//  Created by Alexander Shevtsov on 11.12.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene) // инициализируем окно
+        window?.makeKeyAndVisible() // видимость окна вкл
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+
+
+}
+
