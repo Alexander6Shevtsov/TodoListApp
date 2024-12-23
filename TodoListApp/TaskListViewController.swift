f083b7b1f8dd2fc56be6ff5dd77ee1e452cc1dce
Alexander6Shevtsov <axrsvr@gmail.com>
2024-12-23 14:46:08 +0300

    Error

--- TodoListApp/TaskListViewController.swift
+++ TodoListApp/TaskListViewController.swift
@@ -7,16 +7,61 @@
 
 import UIKit
 
-final class TaskListViewController: UITableViewController {
+protocol NewTaskViewControllerDelegate: AnyObject {
+    func reloadData()
+}
 
+final class TaskListViewController: UITableViewController {
+    
+    private var taskList: [TodoTask] = []
+    private let cellID = "task"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
         view.backgroundColor = .white
         setupNavigationBar()
+        fetchData()
     }
-
+    // переход на View для добавления new task
     @objc private func addNewTask() {
+        let newTaskVC = NewTaskViewController() // создаем экземпляр
+        newTaskVC.delegate = self
+        present(newTaskVC, animated: true) // открываем VC
+    }
+    
+    // MARK: - UITableViewDataSource
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        taskList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) // обьект ячейки
+        let task = taskList[indexPath.row] // извлекаем объект из массива по индексу текущей строки
+        var content = cell.defaultContentConfiguration()
+        content.text = task.title
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    private func fetchData() {
+        guard let appDelegate = (UIApplication.shared.delegate as? AppDelegate) else { return }
         
+        let fetchRequest = TodoTask.fetchRequest() // создаем запос к базе
+        
+        do { // наполняем массив
+            taskList = try  appDelegate.persistentContainer.viewContext.fetch(fetchRequest)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
+
+// MARK: - NewTaskViewControllerDelegate
+extension TaskListViewController: NewTaskViewControllerDelegate {
+    func reloadData() {
+        fetchData()
+        tableView.reloadData()
     }
 }
 
@@ -28,12 +73,8 @@ private extension TaskListViewController {
         
         // Navigation bar appearance
         let navBarAppearance = UINavigationBarAppearance() // конфиг
-        navBarAppearance.backgroundColor = UIColor(
-        red: 21/255,
-        green: 101/255,
-        blue: 192/255,
-        alpha: 194/255
-        )
+        navBarAppearance.backgroundColor = UIColor.milkBlue
+        
         // цвет заголовка в мал состоянии и большом
         navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
         navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
@@ -50,4 +91,10 @@ private extension TaskListViewController {
         // цвет кнопок в NavBar
         navigationController?.navigationBar.tintColor = .white
     }
+}
+
+
+
+#Preview {
+    TaskListViewController()
 }
