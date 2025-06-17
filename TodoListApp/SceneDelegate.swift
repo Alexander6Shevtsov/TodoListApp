24a527ad4d22edc1c0af99f066dcf2ec689e0c42
Alexander6Shevtsov <axrsvr@gmail.com>
2025-06-17 13:08:11 +0300

    Add some comments

--- TodoListApp/SceneDelegate.swift
+++ TodoListApp/SceneDelegate.swift
@@ -12,15 +12,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // вместо _ -> windowScene
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene: windowScene) // инициализируем окно
-        window?.makeKeyAndVisible() // видимость окна вкл          // стартовый NavVC
+        window = UIWindow(windowScene: windowScene) //иниц окно и передаем в параметр 
+        window?.makeKeyAndVisible() // видимость окна вкл
+        // стартовый экран с NavC
         window?.rootViewController = UINavigationController(rootViewController: TaskListViewController())
     }
-
+        // уход в фоновый режим
     func sceneDidEnterBackground(_ scene: UIScene) {
         (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
-
 }
-
